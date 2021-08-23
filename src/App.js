2e732b2d09dffc2f7014a9683c97b48e7c485b8c
Louis_darius <louisdarius96@yahoo.fr>
2021-08-23 19:49:06 +0100

    add home path

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ export default function App() {
       <Router>
         <Nav />
         <Switch>
-          <Route exact path="/" render={() => <Redirect to="/home" />} />
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" render={() => <Redirect to="/" />} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/contact" component={Contact} />
