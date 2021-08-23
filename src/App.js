fab2670fcf14add3a61490913454c68e89c7581e
Louis_darius <louisdarius96@yahoo.fr>
2021-08-23 19:35:14 +0100

    add / path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/home";
 import Register from "./pages/register";
@@ -16,6 +21,7 @@ export default function App() {
       <Router>
         <Nav />
         <Switch>
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
